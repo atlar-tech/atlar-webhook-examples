d83d80dbb80e86f60a976b6b1564c83f5e58eff0
Mattias Appelgren <mattias@ppelgren.se>
2023-04-19 16:24:26 +0200

    Rename java

--- java-spring-boot-webhook/build.gradle.kts
+++ java-spring-boot-webhook/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
 	java
 	id("org.springframework.boot") version "3.0.5"
 	id("io.spring.dependency-management") version "1.1.0"
+	id("com.diffplug.spotless") version "6.18.0"
 }
 
 group = "com.example"
@@ -15,4 +16,14 @@ repositories {
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("commons-codec:commons-codec:1.15")
+}
+
+spotless {
+  java {
+    importOrder()
+    removeUnusedImports()
+    cleanthat()          // has its own section below
+    googleJavaFormat()   // has its own section below
+    formatAnnotations()  // fixes formatting of type annotations, see below
+  }
 }
